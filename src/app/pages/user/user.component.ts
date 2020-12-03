e3ca28e44c22ab10e208bab700b3053897505be0
JavaHater <vanthanhtung1562013@gmail.com>
2020-12-03 16:30:52 +0700

    1st commit of Javahater

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'app/service/user.service';
 
 @Component({
     selector: 'user-cmp',
@@ -7,6 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class UserComponent implements OnInit{
+
+  currentUser = null;
+
+  constructor(
+    public userService: UserService,
+    public route: ActivatedRoute
+  ){}
+
     ngOnInit(){
+      this.getCurrentUser();
+    }
+
+    getCurrentUser(): void {
+      this.userService.getCurrentUser().subscribe(
+        (data) => {
+          this.currentUser = data;
+          debugger
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
 }
